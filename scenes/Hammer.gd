eb0d304763c28413ad328742f8e5882feef83fff
Andrei Jardan <jardan.andrei@gmail.com>
2018-12-25 15:23:17 +0200

    Am schimbat totul dxddxdxdx

--- scenes/Hammer.gd
+++ scenes/Hammer.gd
@@ -2,6 +2,17 @@ extends KinematicBody2D
 
 func _ready():
 	set_physics_process(true)
+	position.y += 50
 
 func _physics_process(delta):
-	rotation += get_angle_to(get_global_mouse_position()) + PI/2
+#	var rotation_increment = get_angle_to(get_global_mouse_position()) + PI/2
+	var rotation_increment = 0.1
+	var myass = position
+	
+	global_transform = global_transform.translated(-myass).rotated(rotation_increment).translated(myass)
+#	get_transform()
+#	rotate(rotation_increment)
+#	var increment = sign(rotation_increment)/10
+#	while !test_move(get_global_transform_with_canvas().rotated(increment), Vector2(0,0)) && abs(rotation_increment) > abs(increment):
+#		rotation_increment -= increment
+#		rotation += increment
---
+++ scenes/Player.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+const SPEED = Vector2(500, 10000)
+const GRAVITY = Vector2(0, -200)
+
+func _ready():
+	set_physics_process(true)
+
+func _physics_process(delta):
+	var player_movement = Vector2(0,0)
+	player_movement.x += int(Input.is_action_pressed("game_right")) - int(Input.is_action_pressed("game_left"))
+	if is_on_floor():
+		player_movement.y -= int(Input.is_action_pressed("game_up"))
+	player_movement.x *= SPEED.x
+	player_movement.y *= SPEED.y
+	
+	var total_movement = player_movement - GRAVITY
+	move_and_slide(total_movement, GRAVITY.normalized())
+	
+	for i in range(get_slide_count()):
+		var collider = get_slide_collision(i).get_collider()
+		print(collider.get_property_list())
+#		if collider.has_meta("strawberry"):
+#			print("aa")
